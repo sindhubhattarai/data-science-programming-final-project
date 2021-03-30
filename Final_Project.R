00021e0d2894faf866d6b6573d87c72ef2e363d4
TravisLloyd <lloydtravis@gmail.com>
2021-03-30 13:12:09 -0700

    Added in house_sales.csv

--- Final_Project.R
+++ Final_Project.R
@@ -3,4 +3,11 @@ library(pacman)
 library(ggplot2)
 
 attach(data)
-head(data)
+## checking where empty values are located.
+colSums(is.na(data))
+bedrooms_nooutliers <- subset(data,bedrooms <=5 & bedrooms >2)
+boxplot(bedrooms_nooutliers)
+summary(boxplot(bedrooms))
+data$sqft_living <- data$sqft_above + data$sqft_basement
+colSums(is.na(data))
+ggplot(data, aes(x = sqft_living, y = bedrooms, color = bathrooms)) + geom_point()
