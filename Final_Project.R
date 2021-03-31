c475d2e11ba1f8bc1c31b984ade15af0a4f44e90
TravisLloyd <lloydtravis@gmail.com>
2021-03-30 20:53:00 -0700

    Added in some filling and boxplots

--- Final_Project.R
+++ Final_Project.R
@@ -1,13 +1,40 @@
 ## Hi Team! This is a test to see if we can use github to collaborate.
 library(pacman)
 library(ggplot2)
+library(dplyr)
+library(tidyr)
 
-attach(data)
+data <- read.csv("house_sales.csv")
+
 ## checking where empty values are located.
 colSums(is.na(data))
-bedrooms_nooutliers <- subset(data,bedrooms <=5 & bedrooms >2)
-boxplot(bedrooms_nooutliers)
-summary(boxplot(bedrooms))
+#fill in sqft_living with basic addition of sqft_above and sqft_basement
 data$sqft_living <- data$sqft_above + data$sqft_basement
+
+
+
+
+data %>% fill(bedrooms, .direction = "up")
+colSums(is.na(data.a))
+data
+boxplot(data$bathrooms)
+##bathroom outlier > 4
+boxplot(data$bedrooms)
+##bedroom outliers <2 and >5
+boxplot(data$sqft_living)
+##sqft_living outliers > 4000
+
+#checking and removing outliers
+data <- subset(data, (bedrooms <=5 & bedrooms >= 2 & sqft_living < 4000))
+colSums(is.na(data))
+
+boxplot(data$sqft_living)
+summary(data$bedrooms)
+colSums(is.na(data))
+ggplot(data, aes(x=sqft_living, y=bedrooms, color = bathrooms)) + geom_point()
+
+
+
+
 colSums(is.na(data))
 ggplot(data, aes(x = sqft_living, y = bedrooms, color = bathrooms)) + geom_point()
