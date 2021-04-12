5dab13f2c75185f41ba1036e11ac7748f7dec7f8
TravisLloyd <lloydtravis@gmail.com>
2021-04-12 11:27:19 -0700

    Cleaned up code a bit and also removed outliers for price as well. There is a graph of price vs sqft_living and colored by bedrooms at the end of the code.

--- Final_Project.R
+++ Final_Project.R
@@ -3,63 +3,46 @@ library(pacman)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(tidyverse)
 
 
 data <- read.csv("house_sales.csv")
 
 ## checking where empty values are located.
 colSums(is.na(data))
+data <- drop(data$living_lot, data$living15_lot15)
 #fill in sqft_living with basic addition of sqft_above and sqft_basement
 data$sqft_living <- data$sqft_above + data$sqft_basement
-
-
-?fill
-fill(data$bedrooms, .direction = "up")
+head(data, 20)
+#fill in bedrooms and bathrooms
+data$bedrooms[which(is.na(data$bedrooms))] <- 2.5
+data$bathrooms[which(is.na(data$bathrooms))] <- 2
+data$sqft_lot[which(is.na(data$sqft_lot))] <- 6500
+data <- subset (data, select = -c(living_lot, living15_lot15))
 colSums(is.na(data))
-data
+
+#missing data is filled in.
+##Moving forward we will look at the outliers of the main categories that we will be inspecting.
+
 boxplot(data$bathrooms)
 ##bathroom outlier > 4
 boxplot(data$bedrooms)
 ##bedroom outliers <2 and >5
 boxplot(data$sqft_living)
 ##sqft_living outliers > 4000
+boxplot(data$price)
+##outliers > 1000000
 
 #checking and removing outliers
-data <- subset(data, (bedrooms <=5 & bedrooms >= 2 & sqft_living < 4000))
-colSums(is.na(data))
+data <- subset(data, (bedrooms <=5 & bedrooms >= 2 & sqft_living < 3700 & bathrooms < 3.5 & price <= 950000))
+boxplot(data$bathrooms)
+boxplot(data$bedrooms)
+boxplot(data$sqft_living)
+boxplot(data$price)
 
 ##Scatterplot built to check bedrooms vs bathrooms by year built
 ggplot(data, aes(sqft_living, bathrooms, color=yr_built)) + geom_point()
 
-##Splitting the data there seems to reveal that newer homes have increasing amount of bathrooms
-h2000 <- subset(data, yr_built > 1995)
-nh2000 <- subset(data, yr_built <= 1995)
 ##data$bathrooms <- data$bathrooms(is.na(data$bathrooms))
-for(i in 1:length(data$bathrooms)) {
-  if(is.na(data$bathrooms[i]) & data$yr_built > 1995) {
-    data$bathrooms <- '3.5'
-    print('1')
-  }
-  else if (is.na(data$bathrooms[i]) & data$yr_built <= 1995) {
-    data$bathrooms <- '2.5'
-    print('0')
-  }
-}
-colSums(is.na(data))
-boxplot(data$bathrooms)
-
-     
-ggplot(h2000, aes(sqft_living, bathrooms, color=yr_built)) + geom_point()
-ggplot(nh2000, aes(sqft_living, bathrooms, color=yr_built)) + geom_point()
-
-
-boxplot(data$sqft_living)
-summary(data$bedrooms)
-colSums(is.na(data))
 ggplot(data, aes(x=sqft_living, y=bedrooms, color = bathrooms)) + geom_point()
-
-
-
-
-colSums(is.na(data))
-ggplot(data, aes(x = sqft_living, y = bedrooms, color = bathrooms)) + geom_point()
+ggplot(data, aes(x = sqft_living, y = price, color = bedrooms)) + geom_point()
